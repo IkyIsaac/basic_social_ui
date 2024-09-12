bbdbe4544b2c3554337bcebb157accc35528db78
IkyIsaac <isaackitomari33@gmail.com>
2024-09-13 02:43:17 +0300

    add rightBar ui

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,9 +1,108 @@
-import { Box } from '@mui/material'
+import { Avatar, AvatarGroup, Box, ImageList, ImageListItem, Typography } from '@mui/material'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
 import React from 'react'
 
 const Rightbar = () => {
   return (
-    <Box flex={2} bgcolor="green" p={2} sx={{ display:{xs:"none", sm:"block"}}} >Rightbar</Box>
+    <Box flex={2} bgcolor="lightgray" p={2} sx={{ display:{xs:"none", sm:"block"}}} >
+      <Typography variant="h6" fontWeight={100}>Online Friends</Typography>
+      <AvatarGroup max={4}>
+  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+  <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+  <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+  <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+  <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+</AvatarGroup>
+<Typography variant="h6" fontWeight={100} mt={2} mb={4} >Latest Photos</Typography>
+
+<ImageList cols={3} rowHeight={164}>
+  {/* sx={{ width: 500, height: 450 }} */}
+    <ImageListItem>
+      <img
+        src="/images/image.png"
+      />
+    </ImageListItem>
+    <ImageListItem>
+      <img
+        src="/images/image.png"
+      />
+    </ImageListItem>
+    <ImageListItem>
+      <img
+        src="/images/image.png"
+      />
+    </ImageListItem>
+</ImageList>
+<Typography variant="h6" fontWeight={100} mt={2} mb={4} >Latest Conversations</Typography> 
+<List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Brunch this weekend?"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                sx={{ color: 'text.primary', display: 'inline' }}
+              >
+                Ali Connors
+              </Typography>
+              {" — I'll be in your neighborhood doing errands this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Summer BBQ"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                sx={{ color: 'text.primary', display: 'inline' }}
+              >
+                to Scott, Alex, Jennifer
+              </Typography>
+              {" — Wish I could come, but I'm out of town this…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Oui Oui"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                sx={{ color: 'text.primary', display: 'inline' }}
+              >
+                Sandra Adams
+              </Typography>
+              {' — Do you have Paris recommendations? Have you ever…'}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+    </List>
+    </Box>
   )
 }
 
