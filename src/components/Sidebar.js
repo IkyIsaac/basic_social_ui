af751bda737cd9ed5469a934c63f6c8a03cddce5
IkyIsaac <isaackitomari33@gmail.com>
2024-09-12 17:40:40 +0300

    add sidebar ui

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,77 @@
-import { Box } from '@mui/material'
+import { Article, DarkMode, Groups, Home, People, Person, Settings, Storefront } from '@mui/icons-material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material'
 import React from 'react'
 
 const Sidebar = () => {
   return (
-    <Box bgcolor="red" flex={1} p={2} sx={{ display:{xs:"none", sm:"block" }}}>Sidebar</Box>
+    <Box flex={1} p={2} sx={{ display:{xs:"none", sm:"block" }}}>
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#'>
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#'>
+              <ListItemIcon>
+                <Article />
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#'>
+              <ListItemIcon>
+                <Groups />
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#'>
+              <ListItemIcon>
+                <People />
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+            </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#'>
+              <ListItemIcon>
+                <Storefront />
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#'>
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#'>
+              <ListItemIcon>
+                <Person />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#'>
+              <ListItemIcon>
+                <DarkMode />
+              </ListItemIcon>
+              <Switch defaultChecked />
+            </ListItemButton>
+          </ListItem>
+      </List>    
+    </Box>
   )
 }
 
